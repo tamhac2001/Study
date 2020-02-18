49533be8450cb563e8ca0137ad247210403d3bcc
Tam Hắc <tamhac2001@gmail.com>
2020-02-18 07:37:34 +0700

    First commit

---
+++ TestGit/src/com/tamhac/testgit/TestGit.java
@@ -0,0 +1,10 @@
+package com.tamhac.testgit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
