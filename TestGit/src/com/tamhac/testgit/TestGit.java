b8f2fefe94a172007c852b60671d98b258101147
Tam Hắc <tamhac2001@gmail.com>
2020-02-18 07:40:17 +0700

    Change something

--- TestGit/src/com/tamhac/testgit/TestGit.java
+++ TestGit/src/com/tamhac/testgit/TestGit.java
@@ -3,8 +3,8 @@ package com.tamhac.testgit;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Git is fun");
+		System.out.println("Git is cool");
 	}
 
 }
